77383bbfd0476fc55dbe825c716667a009f2c9e2
Andreas Mayer <andreas.mayer@inovex.de>
2024-12-06 15:34:10 +0100

    feat: add 2024 day 3 puzzle

---
+++ 2024/src/day3/mull-it-over.kt
@@ -0,0 +1,59 @@
+package day3
+
+import utils.Puzzle
+
+class MullItOver(dir: String) : Puzzle(dir) {
+    override fun solvePuzzleOne(file: List<String>) {
+        var result = 0
+        val regex = Regex("mul\\((\\d+),(\\d+)\\)")
+        for (line in file) {
+            val matches = regex.findAll(line)
+            for (match in matches) {
+                result += match.groupValues[1].toInt() * match.groupValues[2].toInt()
+            }
+        }
+        println("Sum of multiplications: $result")
+    }
+
+    override fun solvePuzzleTwo(file: List<String>) {
+        var result = 0
+        val multiplications = Regex("mul\\((\\d+),(\\d+)\\)")
+        val doConditions = Regex("do\\(\\)")
+        val dontConditions = Regex("don't\\(\\)")
+
+        val oneLiner = file.joinToString("")
+
+        val matches = multiplications.findAll(oneLiner)
+        var cacheIndex = 0
+
+        for (match in matches) {
+            val startIndex = match.range.first
+            val sequence = oneLiner.substring(cacheIndex, startIndex)
+            val allowed = doConditions.findAll(sequence)
+            val disallowed = dontConditions.findAll(sequence)
+
+            var validMatch = true
+            if (disallowed.any()) {
+                validMatch = false
+                cacheIndex = disallowed.last().range.first
+
+                if (allowed.any()) {
+                    if (allowed.last().range.first > disallowed.last().range.first) {
+                        validMatch = true
+                        cacheIndex = match.range.last
+                    }
+                }
+            }
+
+            if (validMatch) {
+                result += match.groupValues[1].toInt() * match.groupValues[2].toInt()
+            }
+        }
+
+        println("Sum of enabled multiplications: $result")
+    }
+}
+
+fun main() {
+    MullItOver("2024/src/day3/").solve()
+}
--- 2024/src/utils/Puzzle.kt
+++ 2024/src/utils/Puzzle.kt
@@ -8,14 +8,17 @@ abstract class Puzzle(dir: String, testFilename: String = "test.txt", puzzleFile
     protected abstract fun solvePuzzleOne(file: List<String>)
     protected abstract fun solvePuzzleTwo(file: List<String>)
 
-    fun solve() {
-        println("Solving example:")
-        solvePuzzleOne(testFile)
-        solvePuzzleTwo(testFile)
-
-        println("\nSolving puzzle input:")
-        solvePuzzleOne(puzzleFile)
-        solvePuzzleTwo(puzzleFile)
+    fun solve(runTest: Boolean = true, runPuzzle: Boolean = true) {
+        if (runTest) {
+            println("Solving example:")
+            solvePuzzleOne(testFile)
+            solvePuzzleTwo(testFile)
+        }
+        if (runPuzzle) {
+            println("\nSolving puzzle input:")
+            solvePuzzleOne(puzzleFile)
+            solvePuzzleTwo(puzzleFile)
+        }
     }
 
 }
